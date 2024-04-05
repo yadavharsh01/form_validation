5eaa9bfe1686b6c6a7290080389d0e32f087f71c
Harsh Yadav <harshyadav.hy20@gmail.com>
2024-04-05 23:39:06 +0530

    js structure

--- script.js
+++ script.js
@@ -16,31 +16,4 @@ const validateInputs=()=>{
     const passwordValue=password.value.trim();
     const cpasswordValue=cpassword.value.trim();
 
-    if(usernameValue===''){
-        setError(username,'Username cannot be blank');
-    }else{
-        setSuccess(username);
     }
-
-    if(emailValue===''){
-        setError(email,'Email cannot be blank');
-    }else if(!isEmail(emailValue)){
-        setError(email,'Email is not valid');
-    }else{
-        setSuccess(email);
-    }
-
-    if(passwordValue===''){
-        setError(password,'Password cannot be blank');
-    }else{
-        setSuccess(password);
-    }
-
-    if(cpasswordValue===''){
-        setError(cpassword,'Confirm Password cannot be blank');
-    }else if(passwordValue!==cpasswordValue){
-        setError(cpassword,'Password does not match');
-    }else{
-        setSuccess(cpassword);
-    }
-}

b3d2e9d4f1e4e491760a1b921a75f46a876807ff
Harsh Yadav <harshyadav.hy20@gmail.com>
2024-04-06 22:53:23 +0530

    all validations added

--- script.js
+++ script.js
@@ -59,3 +59,10 @@ const setSuccess = (element) => {
 const inputControl = element.parentElement;
 const errorDisplay= inputControl.querySelector(".error");
 }
+errorDisplay.innerText="";
+inputControl.classList.remove("error");
+inputControl.classList.add("success");
+
+function isValidEmail(e){
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1-3}\.[0-9]{1-3}\.[0-9]{1-3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(e);
+}

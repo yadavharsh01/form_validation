ca56261246bab2d934c0471faca5bee558415636
Harsh Yadav <harshyadav.hy20@gmail.com>
2024-04-06 17:08:18 +0530

    validations added

--- script.js
+++ script.js
@@ -1,19 +1,61 @@
-const form=document.querySelector('form');
-const username=document.querySelector('#username');
-const email=document.querySelector('#email');
-const password=document.querySelector('#password');
-const cpassword=document.querySelector('#confirm_password');
-const btn=document.querySelector('#btn');
+const form = document.querySelector("form");
+const username = document.querySelector("#username");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const cpassword = document.querySelector("#confirm_password");
+const btn = document.querySelector("#btn");
 
-form.addEventListener('submit',function(e){
-    e.preventDefault();
-    validateInputs();
-})
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  validateInputs();
+});
 
-const validateInputs=()=>{
-    const usernameValue=username.value.trim();
-    const emailValue=email.value.trim();
-    const passwordValue=password.value.trim();
-    const cpasswordValue=cpassword.value.trim();
+const validateInputs = () => {
+  const usernameValue = username.value.trim();
+ const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  const cpasswordValue = cpassword.value.trim();
+};
+if(usernameValue === ""){
+    setError(username, "Username needed !!");
+}
+else{
+    setSucc(username);
+}
 
-    }
+if(emailValue === ""){
+    setError(email, "Email needed !!");
+}
+else{
+    setSuccess(email);
+}
+if(passwordValue === ""){
+    setError(email, "Password needed !!");
+}
+else{
+    setSuccess(password)
+}
+if(cpasswordValue === ""){
+    setError(cpassword, "Same Passeord needed !!");
+}
+else{
+    setSuccess(cpassword);
+}
+if(passwordValue !== cpasswordValue){
+    setError(cpassword, "Password not matched !!");
+}   
+else{
+    setSuccess(cpassword);
+}
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay= inputControl.querySelector(".error");
+  errorDisplay.innerText=message;
+    inputControl.classList.add("error");
+    inputControl.classList.remove("success");
+
+}
+const setSuccess = (element) => {
+const inputControl = element.parentElement;
+const errorDisplay= inputControl.querySelector(".error");
+}

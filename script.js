1e2c8053333b6aaafae5f3a65144ecb827850f01
Harsh Yadav <harshyadav.hy20@gmail.com>
2024-04-07 15:51:35 +0530

    form js added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const form = document.querySelector("form");
+const form = document.querySelector(".form");
 const username = document.querySelector("#username");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
@@ -12,57 +12,56 @@ form.addEventListener("submit", function (e) {
 
 const validateInputs = () => {
   const usernameValue = username.value.trim();
- const emailValue = email.value.trim();
+  const emailValue = email.value.trim();
   const passwordValue = password.value.trim();
   const cpasswordValue = cpassword.value.trim();
 };
-if(usernameValue === ""){
-    setError(username, "Username needed !!");
-}
-else{
-    setSucc(username);
+if (usernameValue === "") {
+  setError(username, "Username needed !!");
+} else {
+  setSuccess(username);
 }
 
-if(emailValue === ""){
-    setError(email, "Email needed !!");
+if (emailValue === "") {
+  setError(email, "Email needed !!");
+} else if (!isValidEmail(emailValue)) {
+  setError(email, "Provide a valid Email ID !!");
+} else {
+  setSuccess(email);
 }
-else{
-    setSuccess(email);
+
+if (passwordValue === "") {
+  setError(password, "Password needed !!");
+} else if (passwordValue.length < 8) {
+  setError(password, "Password should be atleast 8 characters !!");
+} else {
+  setSuccess(password);
 }
-if(passwordValue === ""){
-    setError(email, "Password needed !!");
+
+if (cpasswordValue === "") {
+  setError(cpassword, "please Confirm Password!!");
+} else if (cpasswordValue !=passwordValue) {
+  setError(cpassword, "Password not matched !!");
+} else {
+  setSuccess(cpassword);
 }
-else{
-    setSuccess(password)
-}
-if(cpasswordValue === ""){
-    setError(cpassword, "Same Passeord needed !!");
-}
-else{
-    setSuccess(cpassword);
-}
-if(passwordValue !== cpasswordValue){
-    setError(cpassword, "Password not matched !!");
-}   
-else{
-    setSuccess(cpassword);
-}
+
 const setError = (element, message) => {
   const inputControl = element.parentElement;
-  const errorDisplay= inputControl.querySelector(".error");
-  errorDisplay.innerText=message;
-    inputControl.classList.add("error");
-    inputControl.classList.remove("success");
-
-}
+  const errorDisplay = inputControl.querySelector(".error");
+  errorDisplay.innerText = message;
+  inputControl.classList.add("error");
+  inputControl.classList.remove("success");
+};
 const setSuccess = (element) => {
-const inputControl = element.parentElement;
-const errorDisplay= inputControl.querySelector(".error");
-}
-errorDisplay.innerText="";
-inputControl.classList.remove("error");
-inputControl.classList.add("success");
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
 
-function isValidEmail(e){
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1-3}\.[0-9]{1-3}\.[0-9]{1-3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(e);
+  errorDisplay.innerText = "";
+  inputControl.classList.remove("error");
+  inputControl.classList.add("success");
+};
+function isValidEmail(e) {
+  var reg = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9-]+).([a-z]{2,8})(.[a-z]{2,8})?$/;
+  return reg.test(e);
 }
